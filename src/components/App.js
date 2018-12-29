f55e66db440635f73e85fcba4867bcb2f55dca66
alvics <alvics71@hotmail.com>
2018-12-29 16:56:19 +1000

    API call to unsplash with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class App extends React.Component {
-onSearchSubmit(term) {
-    console.log(term)
-}
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID f4a0afe280c9c4f35a50adf614cd57434ebca86a60c914dca0d637515d8220cc'
+      }
+    });
+    console.log(response.data.results);
+  }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
